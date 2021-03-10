0c5d0a61f30b32d07920d4233c4e5d9f433a6092
Eva Kuntz <kuntzeva@gmail.com>
2021-03-09 20:54:23 -0600

    Adding DatabaseService skeleton server code

---
+++ MicroChat/src/DatabaseService/DatabaseHandler.h
@@ -0,0 +1,37 @@
+#ifndef MICROCHAT_DATABASEHANDLER_H
+#define MICROCHAT_DATABASEHANDLER_H
+
+#include <iostream>
+#include <string>
+#include <regex>
+#include <future>
+
+#include "../../gen-cpp/DatabaseService.h"
+
+#include "../ClientPool.h"
+#include "../ThriftClient.h"
+#include "../logger.h"
+
+namespace microchat
+{
+
+    class DatabaseServiceHandler : virtual public DatabaseServiceIf
+    {
+    public:
+        DatabaseServiceHandler();
+        ~DatabaseServiceHandler() override=default;
+
+        void ping() override;
+    };
+
+    DatabaseServiceHandler::DatabaseServiceHandler();
+
+    void ping()
+    {
+        // Your implementation goes here
+        printf("ping\n");
+    }
+
+} //namespace microchat
+
+#endif //MICROCHAT_DATABASEHANDLER_H
