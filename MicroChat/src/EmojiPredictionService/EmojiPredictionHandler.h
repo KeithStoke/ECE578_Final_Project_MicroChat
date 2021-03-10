5bb38699dd171953ac143d1a1b72a1cd8b74967b
Eva Kuntz <kuntzeva@gmail.com>
2021-03-09 20:42:08 -0600

    Adding EmojiPredictionService skeleton server code

---
+++ MicroChat/src/EmojiPredictionService/EmojiPredictionHandler.h
@@ -0,0 +1,40 @@
+#ifndef MICROCHAT_EMOJIPREDICTIONHANDLER_H
+#define MICROCHAT_EMOJIPREDICTIONHANDLER_H
+
+#include <iostream>
+#include <string>
+#include <regex>
+#include <future>
+
+#include "../../gen-cpp/EmojiPredictionService.h"
+
+#include "../ClientPool.h"
+#include "../ThriftClient.h"
+#include "../logger.h"
+
+namespace microchat{
+
+class EmojiPredictionServiceHandler : virtual public EmojiPredictionServiceIf {
+ public:
+  EmojiPredictionServiceHandler();
+  ~EmojiPredictionServiceHandler() override=default;
+
+  void ping() override;
+  void GetEmoji(Emoji& _return, const std::string& text) override;
+
+};
+
+void ping() {
+    // Your implementation goes here
+    printf("ping\n");
+  }
+
+  void GetEmoji(Emoji& _return, const std::string& text) {
+    // Your implementation goes here
+    printf("GetEmoji\n");
+  }
+
+
+} //namespace microchat
+
+#endif //MICROCHAT_EMOJIPREDICTIONHANDLER_H
